fb45a7990ef19eda2d3054ef7f737189d139287c
Aayush Bhardwaj <aayush993@gmail.com>
2024-06-25 09:10:17 -0400

    2: negative int

--- array.go
+++ array.go
@@ -23,3 +23,32 @@ func MaxSum(arr []int, k int) {
 
 	fmt.Printf("Max sum is %v\n", mSum)
 }
+
+func FirstNegativeInt(arr []int, k int) {
+
+	var result int
+	i := 0
+	var negativeList []int
+
+	for j := 0; j < len(arr); j++ {
+		winSize := j - i + 1
+		if arr[j] < 0 {
+			negativeList = append(negativeList, arr[j])
+		}
+
+		if winSize == k {
+			if len(negativeList) == 0 {
+				result = 0
+			} else {
+				result = negativeList[0]
+
+				if arr[i] == negativeList[0] {
+					negativeList = negativeList[1:] //bug
+				}
+			}
+			fmt.Printf("First negative int is %d\n", result)
+			i++
+		}
+	}
+
+}
